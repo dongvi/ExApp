db6256b6a890248485ecf88ddf387d7e12cc7bef
ViDong <dongvi280201@gmail.com>
2022-03-11 17:10:53 +0700

    build menuBar

--- app/src/main/java/com/example/exprj/MainActivity.java
+++ app/src/main/java/com/example/exprj/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.exprj;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.exprj.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
