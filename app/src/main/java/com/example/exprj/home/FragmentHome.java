31221fa89e6dbd00f86099716f38ced20c0e0839
ViDong <dongvi280201@gmail.com>
2022-03-13 23:35:08 +0700

    done task2: build header for each fragment

--- app/src/main/java/com/example/exprj/home/FragmentHome.java
+++ app/src/main/java/com/example/exprj/home/FragmentHome.java
@@ -2,8 +2,10 @@ package com.example.exprj.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,6 +32,26 @@ public class FragmentHome extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home ,container , false);
 
+        binding.btnPopupMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menuHome(binding);
+            }
+        });
+
         return binding.getRoot();
+    }
+
+    // Xây dựng menu nhỏ cho home
+    void menuHome(FragmentHomeBinding binding){
+        PopupMenu popupMenu = new PopupMenu(getContext(), binding.btnPopupMenu);
+        popupMenu.inflate(R.menu.menu_home);
+
+        Menu menu = popupMenu.getMenu();
+        //set sự kiện cho mỗi chức năng trong menu
+
+        //
+
+        popupMenu.show();
     }
 }
