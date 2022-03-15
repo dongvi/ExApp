dd2ce5e99a2eaecf40e2dc9368b15e92a03574ef
dongvi <90050489+dongvi@users.noreply.github.com>
2022-03-15 11:21:47 +0700

    #6634 Task4 #2: done! create a viewPager hold banners. (#7)

--- app/src/main/java/com/example/exprj/game/FragmentGame.java
+++ app/src/main/java/com/example/exprj/game/FragmentGame.java
@@ -1,6 +1,9 @@
 package com.example.exprj.game;
 
+import static com.example.exprj.home.FragmentHome.banners;
+
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.exprj.AdapterItem;
 import com.example.exprj.R;
@@ -22,6 +26,24 @@ public class FragmentGame extends Fragment {
 
     FragmentGameBinding binding;
     static List<String> menu;
+
+    //auto viewpager
+    private Handler handler;
+    private int timeDelay = 3000;
+    private int page = 0;
+    AdapterPager adapterPager;
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if(adapterPager.getCount() == page)
+                page = 0;
+            else
+                page++;
+            binding.vpgGame.setCurrentItem(page);
+            handler.postDelayed(this, timeDelay);
+        }
+    };
 
     public static FragmentGame newInstance() {
         
@@ -44,7 +66,41 @@ public class FragmentGame extends Fragment {
         binding.rclMenuGame.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
         binding.rclMenuGame.setAdapter(new AdapterItem(menu, R.layout.item_menu_game));
 
+        // Viewpager
+        handler = new Handler();
+        adapterPager = new AdapterPager(getContext(), banners);
+        binding.vpgGame.setAdapter(adapterPager);
+        binding.vpgGame.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                page = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
         return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, timeDelay);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
     }
 
     void addData(){
